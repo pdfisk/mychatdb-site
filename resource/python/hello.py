7d44e5c90f0112aa2523d627a2b64e222ec17c6d
Your Name <you@example.com>
2025-07-25 11:32:48 -0400

    scripts

--- resource/python/hello.py
+++
@@ -1,4 +0,0 @@
-def hello(n):
-    for i in range(1,n+1):
-        print(i, 'Yo DUDE!')
-
---
+++ scripts/gui.py
@@ -0,0 +1,7 @@
+def hello(n):
+    for i in range(1, n + 1):
+        print(i, 'Yo DUDE!')
+
+
+def make_window(caption='a Window', width=525, height=475):
+    print('make_window', caption, width, height)
