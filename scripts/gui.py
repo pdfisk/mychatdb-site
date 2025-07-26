a21f80ca0d9b0bece718b62d3d8807aba6b1e71b
Your Name <you@example.com>
2025-07-26 16:11:09 -0400

    get_globals, set_globals

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,7 +1,45 @@
-def hello(n):
-    for i in range(1, n + 1):
-        print(i, 'Yo DUDE!')
+from browser import window
+
+qxapi = window.QxApi
 
 
-def make_window(caption='a Window', width=525, height=475):
-    print('make_window', caption, width, height)
+def default_caption():
+    return 'a Window'
+
+
+class Window():
+
+    def __init__(self):
+        self.caption = default_caption()
+        self.width = self.default_width()
+        self.height = self.default_height()
+        self.widget = qxapi.make_window(self.caption, self.width, self.height)
+
+    def default_caption(self):
+        return 'a Window'
+
+    def default_height(self):
+        return 275
+
+    def default_width(self):
+        return 235
+
+    def get_height(self):
+        return self.height
+
+    def get_width(self):
+        return self.width
+
+    def hide(self):
+        self.widget.hide()
+
+    def set_height(self, height):
+        self.height = height
+        self.widget.set_height(height)
+
+    def set_width(self, width):
+        self.width = width
+        self.widget.set_width(width)
+
+    def show(self):
+        self.widget.show()
