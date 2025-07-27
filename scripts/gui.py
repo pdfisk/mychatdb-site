774cdc0c11ab15cf899df4e70abe73a6c4ce550c
Your Name <you@example.com>
2025-07-27 14:28:15 -0400

    chat console

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,19 +1,26 @@
 from browser import window
 
 qxapi = window.QxApi
+get_global = qxapi.get_global
+set_global = qxapi.set_global
 
 
-def default_caption():
-    return 'a Window'
+class TranscriptPanel():
+
+    def __init__(self):
+        self.widget = qxapi.make_transcript_panel()
 
 
 class Window():
 
     def __init__(self):
-        self.caption = default_caption()
+        self.widget = qxapi.make_window()
+        self.caption = self.default_caption()
         self.width = self.default_width()
         self.height = self.default_height()
-        self.widget = qxapi.make_window(self.caption, self.width, self.height)
+        self.set_caption(self.caption)
+        self.set_width(self.width)
+        self.set_height(self.height)
 
     def default_caption(self):
         return 'a Window'
@@ -33,13 +40,28 @@ class Window():
     def hide(self):
         self.widget.hide()
 
+    def set_caption(self, caption):
+        self.caption = caption
+        self.widget.setCaption(caption)
+
     def set_height(self, height):
         self.height = height
-        self.widget.set_height(height)
+        self.widget.setHeight(height)
 
     def set_width(self, width):
         self.width = width
-        self.widget.set_width(width)
+        self.widget.setWidth(width)
 
     def show(self):
         self.widget.show()
+
+
+class ChatConsole(Window):
+
+    def __init__(self):
+        super().__init__()
+        self.transcript = TranscriptPanel()
+        self.widget.add(self.transcript.widget, {'edge': 'center'})
+
+    def default_caption(self):
+        return 'Chat Console'
