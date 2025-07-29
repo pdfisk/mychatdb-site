dfee08256dad8f5ce62a5b3e10038470fb1c5628
Your Name <you@example.com>
2025-07-29 16:35:16 -0400

    chat console

--- scripts/gui.py
+++ scripts/gui.py
@@ -5,6 +5,12 @@ get_global = qxapi.get_global
 set_global = qxapi.set_global
 
 
+class SplitPanel():
+
+    def __init__(self):
+        self.widget = qxapi.make_split_panel()
+
+
 class TranscriptPanel():
 
     def __init__(self):
@@ -26,10 +32,10 @@ class Window():
         return 'a Window'
 
     def default_height(self):
-        return 275
+        return 375
 
     def default_width(self):
-        return 235
+        return 425
 
     def get_height(self):
         return self.height
@@ -56,12 +62,61 @@ class Window():
         self.widget.show()
 
 
-class ChatConsole(Window):
+class SplitWindow(Window):
 
     def __init__(self):
         super().__init__()
-        self.transcript = TranscriptPanel()
-        self.widget.add(self.transcript.widget, {'edge': 'center'})
+        self.left_panel = TranscriptPanel()
+        self.right_panel = TranscriptPanel()
+        self.split_panel = SplitPanel()
+        self.split_panel.widget.add(self.left_panel.widget)
+        self.split_panel.widget.add(self.right_panel.widget)
+        self.widget.add(self.split_panel.widget, {'edge': 'center'})
+
+
+class TextWindow(Window):
+
+    def __init__(self):
+        super().__init__()
+        self.text_panel = TranscriptPanel()
+        self.widget.add(self.text_panel.widget, {'edge': 'center'})
+
+
+class ChatConsole(SplitWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.send_btn = qxapi.make_button('Send')
+        self.clear_in_btn = qxapi.make_button('Clear In')
+        self.clear_out_btn = qxapi.make_button('Clear Out')
+        qxapi.add_click_handler(self.send_btn, self.on_send)
+        self.clear_in_btn.addListener('click', self.on_clear_in, self.widget)
+        self.clear_out_btn.addListener('click', self.on_clear_out, self.widget)
+        self.widget.getButtonBar().add(self.send_btn)
+        self.widget.getButtonBar().add(self.clear_out_btn)
+        self.widget.getButtonBar().add(self.clear_in_btn)
 
     def default_caption(self):
         return 'Chat Console'
+
+    def get_left_value(self):
+        return self.left_panel.widget.getValue()
+
+    def get_right_value(self):
+        return self.left_panel.widget.getValue()
+
+    def on_clear_in(self):
+        self.set_left_value('')
+
+    def on_clear_out(self):
+        self.set_right_value('')
+
+    def on_send(self):
+        code = self.get_left_value()
+        self.set_right_value(code)
+
+    def set_left_value(self, value):
+        self.left_panel.widget.setValue(value)
+
+    def set_right_value(self, value):
+        self.right_panel.widget.setValue(value)
